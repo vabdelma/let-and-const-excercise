aa6bfd84987433ccbd544c9aed25ddcf7e4baf12
vabdelma <114247753+vabdelma@users.noreply.github.com>
2022-09-27 17:31:04 -0400

    Add files via upload

---
+++ const pi =3.14.js
@@ -0,0 +1,12 @@
+const pi =3.14
+
+"the difference between var and let is its scope. var can be acessed any where in the function while let can be acessed only in a code block also known as block scope Additionaly let cannot be redeclared"
+
+"the same as let but const cannot be reassassigned "
+
+"the difference between let and const is const cannot be reassigned"
+
+
+"It’s the way that we explain how the JS compiler works. Variables are lifting or “hoisted” to the top of the scope they are declared in. When using var, you can access the variable name and it’s undefined value before it is used later on.
+
+"Function declarations are also hoisted and can be invoked “before” they are defined in a codebase"
